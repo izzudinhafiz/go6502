0264e552e697d1e163b58ee079883a5a8823b252
izzudinhafiz <izzudin.hafiz@gmail.com>
2021-12-22 20:48:50 +0800

    Added memory loading and debug running

--- main.go
+++ main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	cpu6502 "izzudinhafiz.com/go-6502/cpu"
 )
 
 func main(){
-	fmt.Println("Hello World")
-	c := cpu6502.New()
-	c.ModifyMemory(1024*64 - 1, 1)
-	c.PrintMemory()
+	f, err := os.Open("6502_functional_test.bin")
+	if err != nil {
+		panic(err)
+	}
+	stats, statsErr := f.Stat()
+	if statsErr != nil {
+		panic(statsErr)
+	}
+	size := stats.Size()
+	readBuffer := make([]byte, size)
+	f.Read(readBuffer)
+	f.Close()
+
+	fmt.Println(len(readBuffer))
+	cpu := cpu6502.New()
+
+	cpu.WriteMemory(0, readBuffer)
+	fmt.Println(cpu.Memory[0x400 - 10: 0x400+10])
+	cpu.SetResetVector(0x400)
+	cpu.Reset()
+	cpu.Print()
+	cpu.SingleOperation()
+	cpu.Print()
+	cpu.SingleOperation()
+	cpu.Print()
+	cpu.SingleOperation()
+	cpu.Print()
+	cpu.SingleOperation()
+	cpu.Print()
+	cpu.SingleOperation()
+	cpu.Print()
 }
